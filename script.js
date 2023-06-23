3547f630e4f03d009560dcc38c617f9ee33e0052
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-23 17:24:34 -0400

    created a submission

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ const products = [
   { id: 4, name: "Product 4", price: 40 },
   { id: 5, name: "Product 5", price: 50 },
 ];
-
+let cart = []
 // DOM elements
 const productList = document.getElementById("product-list");
-
+const cartList = document.getElementById("cart-list")
 // Render product list
 function renderProducts() {
   products.forEach((product) => {
@@ -22,10 +22,23 @@ function renderProducts() {
 }
 
 // Render cart list
-function renderCart() {}
+function renderCart() {
+ cart = JSON.parse(sessionStorage.getItem("cart"))
+	cart.forEach((product) => {
+    const li = document.createElement("li");
+    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
+    cartList.appendChild(li);
+  });
+}
 
 // Add item to cart
-function addToCart(productId) {}
+function addToCart(productId) {
+	//console.log(productId)
+	let selectedItem = products.find(ele => ele.id==productId)
+	cart.push(selectedItem)
+	sessionStorage.setItem("cart",JSON.stringify(cart))
+}
+
 
 // Remove item from cart
 function removeFromCart(productId) {}
@@ -36,3 +49,12 @@ function clearCart() {}
 // Initial render
 renderProducts();
 renderCart();
+ 
+document.querySelectorAll(".add-to-cart-btn").forEach((btn)=>{
+	btn.addEventListener("click",(event)=>{
+let itemId = event.target.getAttribute("data-id")
+		addToCart(itemId )
+})
+})
+	
+	
